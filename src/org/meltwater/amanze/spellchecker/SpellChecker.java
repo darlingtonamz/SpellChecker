789bd814df342051ee8dfab165102ccdbca418c2
Amanze Ogbonna <darlingtonamz@gmail.com>
2015-10-11 21:07:13 +0000

    Added the console argument reception, and the ability to replace words with the 'correct' method

--- src/org/meltwater/amanze/spellchecker/SpellChecker.java
+++ src/org/meltwater/amanze/spellchecker/SpellChecker.java
@@ -6,62 +6,115 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Hashtable;
 
 public class SpellChecker {
-	static Hashtable<String, String> dictionary;
-	static FileWriter fw;
-	static BufferedWriter bw;
+	private static Hashtable<String, String> dictionary;
+	private static FileWriter fw;
+	private static BufferedWriter bw;
 	
 	public static void main(String[] args){
 		dictionary = new Hashtable<String, String>();
-		loadDictionary();
-		loadFile();
-		//System.out.println(dictionary.get("hiv"));
+		long startTime = System.nanoTime();
+
+		loadDictionary(args[1]);
+		loadFile(args[0]);
+		
+		long endTime = System.nanoTime();
+		long duration = (endTime - startTime);
+		System.out.println("Time spent: " + duration/1000000 + " ms");
 	}
 	
-	static void loadDictionary(){
+	/*
+	 * loads the dictionary provided through the console parameters
+	 */
+	private static void loadDictionary(String dic){
 		try {
-			BufferedReader reader = new BufferedReader(new FileReader(new File("src/org/meltwater/amanze/spellchecker/dictionary")));
+			BufferedReader reader = new BufferedReader(new FileReader(new File("src/org/meltwater/amanze/spellchecker/"+dic)));
 		    String rawLine;
 		    while ((rawLine = reader.readLine()) != null) {
 				String word = rawLine.toLowerCase().trim();
 				dictionary.put(word, word);
 		    }
-		    System.out.println("Done!");
+		    System.out.println("Done reading dictionary: src/org/meltwater/amanze/spellchecker/"+dic);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}	    
 	}
 	
-	static void loadFile(){
+	/*
+	 * returns a permutation of scrambled arrangements of the text which is not in the dictionary
+	 */
+	private final static ArrayList<String> edits(String word) {
+        ArrayList<String> result = new ArrayList<String>();
+        for(int i=0; i < word.length(); ++i) 
+        	result.add(word.substring(0, i) + word.substring(i+1));
+        for(int i=0; i < word.length()-1; ++i) 
+        	result.add(word.substring(0, i) + word.substring(i+1, i+2) + word.substring(i, i+1) + word.substring(i+2));
+        for(int i=0; i < word.length(); ++i) 
+        	for(char c='a'; c <= 'z'; ++c) 
+        		result.add(word.substring(0, i) + String.valueOf(c) + word.substring(i+1));
+        for(int i=0; i <= word.length(); ++i) 
+        	for(char c='a'; c <= 'z'; ++c) 
+        		result.add(word.substring(0, i) + String.valueOf(c) + word.substring(i));
+        return result;
+    }
+	
+	/*The method gets various forms of a word and chooses the one that closely fits the one queried
+	 */
+	private static String correct(String word) {
+        ArrayList<String> list = edits(word);
+        HashMap<String, String> candidates = new HashMap<String, String>();
+        
+        for(String s : list) 
+        	if(dictionary.containsKey(s)) candidates.put(dictionary.get(s),s);
+        
+        if(candidates.size() > 0) 
+        	return candidates.get(Collections.max(candidates.keySet()));
+        
+        for(String s : list) 
+        	for(String w : edits(s)) 
+        		if(dictionary.containsKey(w)) candidates.put(dictionary.get(w),w);
+        
+        return candidates.size() > 0 ? candidates.get(Collections.max(candidates.keySet())) : word;
+    }
+
+	/*
+	 * loads the sample document for spell-checking
+	 */
+	private static void loadFile(String document){
 		try {
 			initOutput();
-			BufferedReader reader = new BufferedReader(new FileReader(new File("src/org/meltwater/amanze/spellchecker/document.txt")));
+			BufferedReader reader = new BufferedReader(new FileReader(new File("src/org/meltwater/amanze/spellchecker/"+document)));
 		    String rawLine;
 		    while ((rawLine = reader.readLine()) != null) {
 				String[] words = rawLine.toLowerCase().split(" ");
 				for (String word : words) {
-					if (dictionary.contains(word)) {
-						//System.out.println(word +" : "+ true);
+					if (dictionary.contains(word) || word.matches(".*[.,()]")) {
 						bw.write(word+" ");
 					}else{
-						bw.write("??? ");
+						bw.write(correct(word)+ "??? ");
 					}
 				}
 				bw.newLine();
 		    }
 
 			bw.close();
-		    System.out.println("Done!");
+		    System.out.println("Done loading document src/org/meltwater/amanze/spellchecker/"+document);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	public static void initOutput(){
+	/*
+	 * The method initializes the FileWriter and BufferedWriter objects for out output
+	 */
+	private static void initOutput(){
 		File file = new File("src/org/meltwater/amanze/spellchecker/document-corrected.txt");
 		try {
 			if (!file.exists())
@@ -72,8 +125,5 @@ public class SpellChecker {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-
-
 	}
 }
